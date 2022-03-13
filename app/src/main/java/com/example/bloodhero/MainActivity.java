c0acdd9ac64fe546813a60aee5e22df6a872a6bc
AbrarKarim01 <abrar.karim@northsouth.edu>
2022-03-14 00:52:27 +0600

    Splash Screen

---
+++ app/src/main/java/com/example/bloodhero/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.bloodhero;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation zoom;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        img = findViewById(R.id.image);
+        img.startAnimation(zoom);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(i);
+                finish();
+            }
+        },4000);
+    }
+}
