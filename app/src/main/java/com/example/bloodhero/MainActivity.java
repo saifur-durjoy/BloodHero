5223b432b1b195c45cd91a0d5f78fd5368655016
AbrarKarim01 <abrar.karim@northsouth.edu>
2022-03-15 22:59:43 +0600

    icon

--- app/src/main/java/com/example/bloodhero/MainActivity.java
+++ app/src/main/java/com/example/bloodhero/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View decorView;
     Animation zoom;
     ImageView img;
 
@@ -18,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+            if (visibility == 0)
+                decorView.setSystemUiVisibility(hideSystemBars());
+        });
         zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
         img = findViewById(R.id.image);
         img.startAnimation(zoom);
@@ -28,5 +35,28 @@ public class MainActivity extends AppCompatActivity {
             startActivity(i);
             finish();
         },4000);
+
     }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            decorView.setSystemUiVisibility(hideSystemBars());
+        }
+
+    }
+
+    private int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+               | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                   | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+
+
+
+    }
+
+
 }
