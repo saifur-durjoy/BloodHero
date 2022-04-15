28113548c4495964bab9b6a3b03f4eeb4cd7c624
arfana-rahman <arfanarahman1998@gmail.com>
2022-04-15 13:33:39 +0600

    Added profile layout.

--- app/src/main/java/com/example/bloodhero/ProfileActivity.java
+++ app/src/main/java/com/example/bloodhero/ProfileActivity.java
@@ -1,14 +1,62 @@
 package com.example.bloodhero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView type, name, email, idNumber, phoneNumber, bloodGroup;
+    private CircleImageView profileImage;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+
+        type = findViewById(R.id.type);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        idNumber = findViewById(R.id.idNumber);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        bloodGroup = findViewById(R.id.bloodGroup);
+        profileImage = findViewById(R.id.profileImage);
+        backButton = findViewById(R.id.backButton);
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
