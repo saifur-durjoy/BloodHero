f9f22f17a5fb3d856a9e98a81a1aaa6eeeed9a10
saifur-durjoy <saifur.durjoy@gmail.com>
2022-04-15 13:01:12 +0600

    Revert "added main menu drawer"
    This reverts commit 44348483

--- app/src/main/java/com/example/bloodhero/MainActivity.java
+++ app/src/main/java/com/example/bloodhero/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.bloodhero;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -10,12 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.google.android.material.navigation.NavigationView;
-
-import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,11 +18,6 @@ public class MainActivity extends AppCompatActivity {
      private DrawerLayout drawerLayout;
      private Toolbar toolbar;
      private NavigationView nav_view;
-
-     private CircleImageView nav_profile_image;
-     private TextView nav_fullname, nav_email, nav_bloodgroup, nav_type;
-
-     private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,45 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
-        nav_profile_image = nav_view.getHeaderView(0).findViewById(R.id.nav_user_image);
-        nav_fullname = nav_view.getHeaderView(0).findViewById(R.id.nav_user_fullname);
-        nav_email = nav_view.getHeaderView(0).findViewById(R.id.nav_user_email);
-        nav_bloodgroup = nav_view.getHeaderView(0).findViewById(R.id.nav_user_bloodgroup);
-        nav_type = nav_view.getHeaderView(0).findViewById(R.id.nav_user_type);
-
-        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(
-                FirebaseAuth.getInstance().getCurrentUser().getUid()
-        );
-
-        userRef.addValueEventListener(new ValueEventListener(){
-            @override
-            public void onDataChange(@NonNull DataSnapshot snapshot){
-                if(snapshot.exist()){
-                    String name = snapshot.child("name").getValue.toString();
-                    nav_fullname.setText(name);
-
-                    String email = snapshot.child("email").getValue.toString();
-                    nav_email.setText(email);
-
-                    String bloodgroup = snapshot.child("bloodgroup").getValue.toString();
-                    nav_bloodgroup.setText(bloodgroup);
-
-                    String type = snapshot.child("type").getValue.toString();
-                    nav_type.setText(type);
-
-                    String imageUrl = snapshot.child("profilepictureurl").getValue.toString();
-                    Glide.with(getApplicationContext()).load(imageUrl).into(nav_profile_image);
-                    
-                }
-            }
-
-            @override
-            public void onCancelled(@NonNull DatabaseError error){
-
-            }
-        });
-
 
     }
 }
