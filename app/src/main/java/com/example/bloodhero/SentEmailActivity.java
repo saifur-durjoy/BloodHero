a47ef2c03ab54c33723c6f10ce4ebad8d002c3df
Arfana-Rahman <arfanarahman1998@gmail.com>
2022-04-15 23:47:32 +0600

    Created Retrieve Users List.

--- app/src/main/java/com/example/bloodhero/SentEmailActivity.java
+++ app/src/main/java/com/example/bloodhero/SentEmailActivity.java
@@ -1,11 +1,19 @@
 package com.example.bloodhero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,5 +50,56 @@ public class SentEmailActivity extends AppCompatActivity {
         userAdapter = new UserAdapter(SentEmailActivity.this, userList);
         recyclerView.setAdapter(userAdapter);
 
+        idList = new ArrayList<>();
+        getIdOfUsers();
+
+    }
+
+    private void getIdOfUsers() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("emails")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
+
+      reference.addValueEventListener(new ValueEventListener() {
+          @Override
+          public void onDataChange(@NonNull DataSnapshot snapshot) {
+              idList.clear();
+              for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                  idList.add(dataSnapshot.getKey());
+              }
+              showUsers();
+          }
+
+          @Override
+          public void onCancelled(@NonNull DatabaseError error) {
+
+          }
+      });
+    }
+
+    private void showUsers() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("user");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user = dataSnapshot.getValue(User.class);
+
+                    for (String id : idList){
+
+                        if(user.getId().equals(id)){
+                            userList.add(user);
+                        }
+                    }
+                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
