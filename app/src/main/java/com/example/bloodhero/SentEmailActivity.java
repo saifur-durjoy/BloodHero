6699ce6ca8d6b816f16b3ea08dc874df5f9f07c1
Arfana-Rahman <arfanarahman1998@gmail.com>
2022-04-15 22:11:10 +0600

    Toolbar and RecycleView For Sent Email Activity.

--- app/src/main/java/com/example/bloodhero/SentEmailActivity.java
+++ app/src/main/java/com/example/bloodhero/SentEmailActivity.java
@@ -1,13 +1,46 @@
 package com.example.bloodhero;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SentEmailActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+
+    List<String> idList;
+    List<User> userList;
+    UserAdapter userAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sent_email);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("people sent Emails");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        recyclerView = findViewById(R.id.recycleView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        userList = new ArrayList<>();
+        userAdapter = new UserAdapter(SentEmailActivity.this, userList);
+        recyclerView.setAdapter(userAdapter);
+
     }
 }
