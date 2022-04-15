32e5188d6b4e8fbd6a4d9025061981a609107f24
Arfana-Rahman <arfanarahman1998@gmail.com>
2022-04-15 19:19:06 +0600

    completed profile tab

--- app/src/main/java/com/example/bloodhero/ProfileActivity.java
+++ app/src/main/java/com/example/bloodhero/ProfileActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -40,9 +44,42 @@ public class ProfileActivity extends AppCompatActivity {
         profileImage = findViewById(R.id.profileImage);
         backButton = findViewById(R.id.backButton);
 
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users").child(
+
+                FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
+
+        reference.addValueEventListener(new ValueEventListener(){
+            @override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+                if(snapshot.exist()){
+                    type.setText(snapshot.child("type").getValue().toString());
+                    name.setText(snapshot.child("name").getValue().toString());
+                    idNumber.setText(snapshot.child("idnumber").getValue().toString());
+                    phoneNumber.setText(snapshot.child("phonenumber").getValue().toString());
+                    bloodGroup.setText(snapshot.child("bloodgroup").getValue().toString());
+                    email.setText(snapshot.child("email").getValue().toString());
+
+                    Glide.with(getApplicationContext()).load(snapshot.child("profilepictureurl").getValue().toString()).into(profileImage)
 
 
 
+                }
+            }
+            @override
+            public void onCancelled(@NonNull DatabaseError error){
+
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
 
