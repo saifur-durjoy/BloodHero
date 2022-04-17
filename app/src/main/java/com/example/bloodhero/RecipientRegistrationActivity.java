371369a80b2081727d2d6f1cb3c45a121a38f700
AbrarKarim01 <abrar.karim@northsouth.edu>
2022-04-18 04:51:56 +0600

    Registration Done

--- app/src/main/java/com/example/bloodhero/RecipientRegistrationActivity.java
+++ app/src/main/java/com/example/bloodhero/RecipientRegistrationActivity.java
@@ -2,30 +2,50 @@ package com.example.bloodhero;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class RecipientRegistrationActivity extends AppCompatActivity {
 
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipient_registration);
 
-        TextView backButton;
-        backButton = findViewById(R.id.backButton);
-        backButton.setOnEditorActionListener((textView, i, keyEvent) -> {
-            Intent intent = new Intent(RecipientRegistrationActivity.this, LoginActivity.class);
-            RecipientRegistrationActivity.this.startActivity(intent);
+        View backButton = findViewById(R.id.backButton);
 
-            return false;
+        backButton.setOnClickListener(view -> {
+            Intent intent = new Intent (RecipientRegistrationActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+            if (visibility == 0)
+                decorView.setSystemUiVisibility(hideSystemBars());
+        });
+
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            decorView.setSystemUiVisibility(hideSystemBars());
         }
 
+    }
 
-        );
-
+    private int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
 
 
 
