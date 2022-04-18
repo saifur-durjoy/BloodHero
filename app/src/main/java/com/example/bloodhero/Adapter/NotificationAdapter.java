ab43a51ebf6fd436c4eb19b0295b17c4eb382d6a
arfana-rahman <arfanarahman1998@gmail.com>
2022-04-18 22:20:30 +0600

    Successfully complete the Notification Activity Tab.

--- app/src/main/java/com/example/bloodhero/Adapter/NotificationAdapter.java
+++ app/src/main/java/com/example/bloodhero/Adapter/NotificationAdapter.java
@@ -1,5 +1,7 @@
 package com.example.bloodhero.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,24 +9,53 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.bloodhero.R;
+import com.example.bloodhero.model.Notification;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.ViewHolder>{
 
+    private Context context;
+    private List<Notification> notificationList;
+
+
+    public NotificationAdapter(Context context, List<Notification> notificationList) {
+        this.context = context;
+        this.notificationList = notificationList;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.notification_item,parent,false);
+        return new NotificationAdapter.ViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Notification notification = notificationList.get(position);
+
+        holder.notification_text.setText(notification.getText());
+        holder.notification_date.setText(notification.getDate());
+
+        getUserInfo(holder.notification_profile_image, holder.notification_name, notification.getSenderId());
 
     }
 
+
+
     @Override
     public int getItemCount() {
-        return 0;
+        return notificationList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
@@ -41,6 +72,23 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
             notification_text = itemView.findViewById(R.id.notification_text);
             notification_date = itemView.findViewById(R.id.notification_date);
         }
+    }
+
+    private void getUserInfo(final CircleImageView circleImageView, final TextView nameTextView, final String senderId) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users").child(senderId);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                nameTextView.setText(user.getName());
+                Glide.with(context).load(user.getProfilepictureurl()).into(circleImageView)
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/bloodhero/NotificationActivity.java
@@ -0,0 +1,94 @@
+package com.example.bloodhero;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.bloodhero.Adapter.NotificationAdapter;
+import com.example.bloodhero.model.Notification;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+
+    List<Notification> notificationList;
+    NotificationAdapter notificationAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Notifications");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        recyclerView = findViewById(R.id.recycleView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        notificationList = new ArrayList<>();
+        notificationAdapter = new NotificationAdapter(NotificationActivity.this, notificationList);
+        recyclerView.setAdapter(notificationAdapter);
+
+        readNotifications();
+
+
+    }
+
+    private void readNotifications() {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("notifications").child(
+
+                FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
+
+        reference.addValueEventListener(new ValueEventListener(){
+            @override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+
+                notificationList.clear();
+                for (DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Notification notification = dataSnapshot.getValue(Notification.class);
+                    notificationList.add(notification);
+
+                }
+                notificationAdapter.notifyDataSetChanged();
+            }
+
+
+            @override
+            public void onCancelled(@NonNull DatabaseError error){
+
+            }
+    });
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
