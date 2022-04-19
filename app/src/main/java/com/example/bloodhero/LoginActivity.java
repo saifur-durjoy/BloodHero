9adb5a8363d1c3c674426059a3b6d4c287d8c5e9
AbrarKarim01 <abrar.karim@northsouth.edu>
2022-04-19 18:19:00 +0600

    Sign in user

--- app/src/main/java/com/example/bloodhero/LoginActivity.java
+++ app/src/main/java/com/example/bloodhero/LoginActivity.java
@@ -1,24 +1,103 @@
 package com.example.bloodhero;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private View decorView;
+    private TextInputEditText loginEmail, loginPassword;
+    private TextView forgotPassword;
+    private Button loginButton;
+    private ProgressDialog loader;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if(user != null){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity1.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        };
+
         TextView backButton = findViewById(R.id.backButton);
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        forgotPassword = findViewById(R.id.forgotpassword);
+        loginButton = findViewById(R.id.loginButton);
+        loader = new ProgressDialog(this);
+
 
         backButton.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, SelectRegistrationActivity.class);
             startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = loginEmail.getText().toString().trim();
+                final String password = loginPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    loginEmail.setError("Email is Required");
+                }
+                if (TextUtils.isEmpty(password)){
+                    loginPassword.setError("Password is Required");
+                }
+
+                else{
+                     loader.setMessage("Logging In");
+                     loader.setCanceledOnTouchOutside(false);
+                     loader.show();
+
+                     mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                         @Override
+                         public void onComplete(@NonNull Task<AuthResult> task) {
+                             if (task.isSuccessful()){
+                                 Toast.makeText(LoginActivity.this, "Log in Successful", Toast.LENGTH_SHORT).show();
+                                 Intent intent = new Intent(LoginActivity.this, MainActivity1.class);
+                                 startActivity(intent);
+                                 finish();
+                             }
+                             else {
+                                 Toast.makeText(LoginActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                             }
+
+                             loader.dismiss();
+
+                         }
+                     });
+                }
+            }
         });
 
         decorView = getWindow().getDecorView();
@@ -48,4 +127,18 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+
+    protected void onStop(){
+        super.onStop();
+        mAuth.removeAuthStateListener(authStateListener);
+    }
+
 }
--- app/src/main/java/com/example/bloodhero/MainActivity1.java
+++ app/src/main/java/com/example/bloodhero/MainActivity1.java
@@ -2,13 +2,30 @@ package com.example.bloodhero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity1 extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(MainActivity1.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
